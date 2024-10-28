70331df4d9881fc57a7bb226d9e4cf5df308bda7
luis cahuana <luisrolandocahuanaleon@gmail.com>
2024-10-28 10:56:24 +0100

    Agregar archivos de proyecto.

---
+++ WpfApp6/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using devDept.Eyeshot;
+using devDept.Eyeshot.Entities;
+using devDept.Geometry;
+
+using System.Windows;
+
+using Line = devDept.Eyeshot.Entities.Line;
+using Region = devDept.Eyeshot.Entities.Region;
+
+
+namespace WpfApp6
+{
+    /// <summary>  
+    /// Interaction logic for MainWindow.xaml  
+    /// </summary>  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            CompositeCurve profile = new CompositeCurve(
+                new Line(0, 0, 50, 0),
+                new Line(50, 0, 50, 50),
+                new Line(50, 50, 0, 50),
+                new Line(0, 50, 0, 0)
+            );
+
+            Region region = new Region(profile);
+
+            Brep body = region.ExtrudeAsBrep(20);
+
+            Region holeRegion = new Region(new Circle(Plane.XY, new Point3D(25, 25, 0), 10));
+
+            body.ExtrudeRemove(holeRegion, 20);
+
+
+            design1.Entities.Add(body, System.Drawing.Color.Blue);
+
+            design1.SetView(viewType.Trimetric);
+            design1.ZoomFit();
+            design1.Invalidate();
+        }
+    }
+}
